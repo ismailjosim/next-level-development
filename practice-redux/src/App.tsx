6c31cd2bcb1cc0e625b59ef5eb8aead5abdc3021
ismailjosim <ismailjosim99@gmail.com>
2025-08-01 16:02:41 +0600

    refactor: restructure project by removing Redux and implementing new UI components
    - Updated package.json to include new dependencies for UI components and removed Redux-related packages.
    - Refactored App component to use a new Button component instead of Redux state management.
    - Removed counterSlice, store, and custom hooks related to Redux.
    - Added utility functions for class name management.
    - Introduced new CSS variables and themes for styling.
    - Updated TypeScript configuration for better module resolution and paths.
    - Migrated Vite configuration to TypeScript and added path aliasing.
    - Created a new Button component with variants and utility classes.

--- practice-redux/src/App.tsx
+++ practice-redux/src/App.tsx
@@ -1,35 +1,11 @@
-import { decrement, increment, incrementByPayload } from './redux/features/counter/counterSlice'
-import { useAppDispatch, useAppSelector } from './redux/hook'
+import { Button } from './components/ui/button';
 
 const App = () => {
-	const count = useAppSelector((state) => state.counter.value)
-	const dispatch = useAppDispatch()
-	return (
-		<div>
-			<h3>Counter Application</h3>
-			<section className='flex justify-center items-center gap-5'>
-				<button
-					className='bg-amber-700 cursor-pointer px-5 pb-5 pt-3 rounded-2xl text-2xl'
-					onClick={() => dispatch(incrementByPayload(5))}
-				>
-					+
-				</button>
-				<button
-					className='bg-amber-700 cursor-pointer px-5 pb-5 pt-3 rounded-2xl text-2xl'
-					onClick={() => dispatch(increment())}
-				>
-					+
-				</button>
-				<p className='text-2xl font-bold'>{count}</p>
-				<button
-					className='bg-amber-700 cursor-pointer px-5 pb-5 pt-3 rounded-2xl text-2xl'
-					onClick={() => dispatch(decrement())}
-				>
-					-
-				</button>
-			</section>
-		</div>
-	)
-}
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- practice-redux/src/main.tsx
+++ practice-redux/src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import App from './App.tsx'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-	throw new Error("Root element with id 'root' not found");
-}
-createRoot(rootElement).render(
-	<StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
