80127093d5c7bbad0b4927c43259730fadeaa256
ismailjosim <ismailjosim99@gmail.com>
2025-09-04 20:03:17 +0600

    Creating Blogs Using Next.js Form Component

---
+++ nextjs/nexa-blog-with-nextjs/src/app/blogs/create/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BlogForm from '../../../components/ui/BlogForm'
+
+const CreateBlogPage = () => {
+	return (
+		<div>
+			<h3>Create Blog Page</h3>
+			<BlogForm />
+		</div>
+	)
+}
+
+export default CreateBlogPage
